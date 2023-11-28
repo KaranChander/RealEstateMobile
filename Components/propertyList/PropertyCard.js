413cc6f0318c750f85247a55fe9963b8fcb7b864
Nagarjuna Rapolu <112997527+rapolunagarjuna@users.noreply.github.com>
2023-11-27 20:15:34 -0500

    Update PropertyCard.js

--- Components/propertyList/PropertyCard.js
+++ Components/propertyList/PropertyCard.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const PropertyCard = ({ property }) => {
+const PropertyCard = ({ property, onPress }) => {
   return (
-    <View style={styles.card}>
-       <Image style={styles.image} source={{ uri: property.primary_photo?.href }} resizeMode="cover"/>
-      <View style={styles.textContainer}>
-      <View style={styles.row}>
-          <Text style={styles.title}>{"$"+(property.list_price).toLocaleString()}</Text>
-          <Text style={styles.cashFlow}>{"$387/mo Cash Flow"}</Text>
+    <TouchableOpacity onPress={onPress} style={styles.card}>
+      <View style={styles.card}>
+         <Image style={styles.image} source={{ uri: property.primary_photo?.href }} resizeMode="cover"/>
+        <View style={styles.textContainer}>
+        <View style={styles.row}>
+            <Text style={styles.title}>{"$"+(property.list_price).toLocaleString()}</Text>
+            <Text style={styles.cashFlow}>{"$387/mo Cash Flow"}</Text>
+          </View>
+        <Text style={styles.details}>{property.location?.address?.line + " " + property.location?.address?.city + " " + property.location?.address?.state + " " + property.location?.address?.postal_code}</Text>
+        <View style={styles.row}>
+          <Icon name="bed" size={20} color="#808080" />
+          <Text style={styles.details}>{property.description.beds} beds</Text>
+          <Icon name="water" size={20} color="#808080" style={styles.icon} />
+          <Text style={styles.details}>{property.description.baths} baths</Text>
         </View>
-      <Text style={styles.details}>{property.location?.address?.line + " " + property.location?.address?.city + " " + property.location?.address?.state + " " + property.location?.address?.postal_code}</Text>
-      <View style={styles.row}>
-        <Icon name="bed" size={20} color="#808080" />
-        <Text style={styles.details}>{property.description.beds} beds</Text>
-        <Icon name="water" size={20} color="#808080" style={styles.icon} />
-        <Text style={styles.details}>{property.description.baths} baths</Text>
+        <Text style={styles.details}>{(property.description?.lot_sqft??0).toLocaleString()} sq ft</Text> 
+        </View> 
       </View>
-      <Text style={styles.details}>{(property.description?.lot_sqft??0).toLocaleString()} sq ft</Text> 
-      </View> 
-    </View>
+    </ TouchableOpacity>
   );
 };
 
